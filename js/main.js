96596a8748f7d844922c550e30d28a7a816baf58
ibrahim ouachem <b.ouachem@icloud.com>
2017-03-30 20:20:17 -0700

    sound keyboard

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return //stop the function from ranning all together
+  audio.currentTime = 0 //rewind to the start
+  audio.play()
+  key.classList.add('playing')
+}
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return //skip if it's not a transform
+	this.classList.remove("playing")
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener("keydown", playSound)
+
+
+// function removeTransition(e) {
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+//   }
+
+//   function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+
+//     key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+//   }
+
+//   const keys = Array.from(document.querySelectorAll('.key'));
+//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//   window.addEventListener('keydown', playSound);
